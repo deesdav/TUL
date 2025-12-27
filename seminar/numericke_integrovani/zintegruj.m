a54320c71e485876e6cd251227bb486af2eb0ad3
David Švancar <dejvisvan@seznam.cz>
2025-12-27 20:33:39 +0100

    Deployed e18a011 with MkDocs version: 1.6.1

--- seminar/numericke_integrovani/zintegruj.m
+++ seminar/numericke_integrovani/zintegruj.m
@@ -1,7 +1,7 @@
 function z = zintegruj(x,y,c)
 z(1) = c;
 for i = 1:length(x)-1
-    z(i+1) = z(i) + 0.5 * (x(i+1)-x(i) * y(i) + y(i+1));
+    z(i+1) = z(i) + 0.5 * (x(i+1)-x(i)) * (y(i) + y(i+1));
 end
 end
 
