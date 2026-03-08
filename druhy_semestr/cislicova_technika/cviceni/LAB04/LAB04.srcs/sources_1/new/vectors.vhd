4d06e7ac800688def4f76ba6265d86fdf4b42a50
David Švancar <dejvisvan@seznam.cz>
2026-03-08 10:34:51 +0100

    Deployed 399b217 with MkDocs version: 1.6.1

---
+++ druhy_semestr/cislicova_technika/cviceni/LAB04/LAB04.srcs/sources_1/new/vectors.vhd
@@ -0,0 +1,16 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity vectors is
+    Port (
+        switches : in std_logic_vector(15 downto 0);
+        LEDs : out std_logic_vector(15 downto 0)
+    );
+end vectors;
+
+architecture Behavioral of vectors is
+
+begin
+
+
+end Behavioral;
