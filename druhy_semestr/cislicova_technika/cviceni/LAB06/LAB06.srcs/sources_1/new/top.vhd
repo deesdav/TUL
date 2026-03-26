fa17d2d0b11f665dee8148251b6d50148c11126b
David Švancar <dejvisvan@seznam.cz>
2026-03-26 19:45:02 +0100

    Deployed 5cd08f8 with MkDocs version: 1.6.1

--- druhy_semestr/cislicova_technika/cviceni/LAB06/LAB06.srcs/sources_1/new/top.vhd
+++ druhy_semestr/cislicova_technika/cviceni/LAB06/LAB06.srcs/sources_1/new/top.vhd
@@ -12,8 +12,34 @@ architecture Behavioral of top is
 
 begin
 
-    -- insert two muxes here
+    -- insert two muxes here  
+	mux1_inst : entity work.multiplexor
+    port map(
+        d => SW(3 downto 0),
+        s => SW(5 downto 4),
+        q => LED(0)
+    );			   
+	
+	mux2_inst : entity work.multiplexor
+    port map(
+        d => SW(3 downto 0),
+        s => SW(7 downto 6),
+        q => LED(1)
+    );
+	
     
-    -- insert a decoder here
+    -- insert a decoder here	
+	dec1_inst : entity work.decoder
+    port map(
+        d => SW(9 downto 8),
+        q => LED(5 downto 2)
+    );
+	
+	dec2_inst : entity work.decoder
+    port map(
+        d => SW(11 downto 10),
+        q => LED(9 downto 6)
+    );
+	
 
 end Behavioral;
